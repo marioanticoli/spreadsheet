f2291905a7b0159f6dae94e99513909b3e52cafc
mario <mario@imrepublic.com>
2021-02-16 22:38:14 +0100

    factory config

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ socketio = SocketIO()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
 
     db.init_app(app)
     migrate.init_app(app, db)
